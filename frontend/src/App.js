a6bd51c329048fd375b9fbc47d939ba34c7168ea
Dan Humphreys <dan.humphreys@xde.dev>
2024-04-14 22:16:10 +0100

    Added amplify deploy to Github Actions.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
+        <button onClick={() => sendEvent("https://bbc.co.uk")}>Send event</button>
+        </header>
     </div>
   );
 }
+
+const sendEvent = (url) => {
+  window.open(url, "_blank", "noreferrer");
+};
 
 export default App;

8163c3a73cc6c353e7adfc2d9eb3747968a34456
Dan Humphreys <dan.humphreys@xde.dev>
2024-06-05 22:18:46 +0100

    Testing service.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,16 +5,12 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse
 import com.amazonaws.services.lambda.runtime.Context
 
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Handler : RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     override fun handleRequest(input: APIGatewayV2HTTPEvent, context: Context): APIGatewayV2HTTPResponse {
         return APIGatewayV2HTTPResponse().apply {
             statusCode = 200
             headers = mapOf("Content-Type" to "application/json")
-            body = "hello all again"
+            body = "hello all again!this has been changed"
         }
     }
 }
